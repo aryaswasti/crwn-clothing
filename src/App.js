5018cbc468577129dc74855d6772143d0eacc67c
Swasti <swastiarya4@gmail.com>
2021-05-25 12:08:16 +0530

    adding final firebase files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from '../src/pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from './firebase/firebase.util'
+import {auth, createUserProfileDocument} from './firebase/firebase.util'
 
 class App extends React.Component {
   constructor(props) {
@@ -20,10 +20,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState( { currentUser: user});
-      console.log()
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async  userAuth=> {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot =>
+        this.setState({
+          currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        }))
+      } else {
+        this.setState({currentUser: userAuth});
+      }
+    });
   }
 
   componentWillUnmount() {
